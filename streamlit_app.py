951050c1052ad704d84398a3a3a015ac7ea21026
Enock Wafula Bereka <enochosenwafulah@gmail.com>
2025-05-25 02:19:41 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,5 @@ import numpy as np
 st.title('Stroke Risk Prediction Dashboard')
 
 st.write('This Tool assesses stroke risk using machine learning integration')
-df = pd.read_csv('https://github.com/Biosticianenoch/data/blob/main/breast%20cancer.csv')
-df
+
  
