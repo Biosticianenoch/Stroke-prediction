9f75df519008d8d23c51b93eabafd6deec90927c
Enock Wafula Bereka <enochosenwafulah@gmail.com>
2025-05-25 01:53:39 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st 
 import pandas as pd 
 import numpy as np 
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"]) 
-st.line_chart(chart_data)
+df = pd.DataFrame( 
+np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], 
+columns=["lat", "lon"], 
+) 
+st.map(df)
