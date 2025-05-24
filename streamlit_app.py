0dad14642712b95bedafa4b633af2866b8712a57
Enock Wafula Bereka <enochosenwafulah@gmail.com>
2025-05-25 01:58:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,2 @@
 import streamlit as st 
-left, middle, right = st.columns(3) 
-if left.button("Plain button", use_container_width=True): 
-left.markdown("You clicked the plain button.") 
-if middle.button("Emoji button", icon="     ", use_container_width=True): 
-middle.markdown("You clicked the emoji button.") 
-if right.button("Material button", icon=":material/mood:", use_container_width=True): 
-right.markdown("You clicked the Material button.")
+st.title("Stroke Prediction Tool")
